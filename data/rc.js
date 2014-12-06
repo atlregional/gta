93b5858b04641978ce57ce6ef6733cfc09cee0d5
Landon Reed <landontreed@gmail.com>
2014-12-06 15:33:09 -0500

    major update to gta

---
+++ data/rc.js
@@ -0,0 +1,12 @@
+var rc_data = [{"id":3,"name":"Atlanta Regional Commission","counties":["Cherokee","Clayton","Cobb","DeKalb","Douglas","Fayette","Fulton","Gwinnett","Henry","Rockdale"]},
+{"id":7,"name":"Central Savannah River Area","counties":["Burke","Columbia","Glascock","Hancock","Jefferson","Jenkins","Lincoln","McDuffie","Richmond","Taliaferro","Warren","Washington","Wilkes"]},
+{"id":12,"name":"Coastal Regional Commission","counties":["Bryan","Bulloch","Camden","Chatham","Effingham","Glynn","Liberty","Long","McIntosh","Screven"]},
+{"id":2,"name":"Georgia Mountains","counties":["Banks","Dawson","Forsyth","Franklin","Habersham","Hall","Hart","Lumpkin","Rabun","Stephens","Towns","Union","White"]},
+{"id":9,"name":"Heart of Georgia Altamaha","counties":["Appling","Bleckley","Candler","Dodge","Emanuel","Evans","Jeff Davis","Johnson","Laurens","Montgomery","Tattnall","Telfair","Toombs","Treutlen","Wayne","Wheeler","Wilcox"]},
+{"id":6,"name":"Middle Georgia","counties":["Baldwin","Bibb","Crawford","Houston","Jones","Monroe","Peach","Pulaski","Putnam","Twiggs","Wilkinson"]},
+{"id":5,"name":"Northeast Georgia","counties":["Barrow","Clarke","Elbert","Greene","Jackson","Jasper","Madison","Morgan","Newton","Oconee","Oglethorpe","Walton"]},
+{"id":1,"name":"Northwest Georgia","counties":["Bartow","Catoosa","Chattooga","Dade","Fannin","Floyd","Gilmer","Gordon","Haralson","Murray","Paulding","Pickens","Polk","Walker","Whitfield"]},
+{"id":8,"name":"River Valley","counties":["Chattahoochee","Clay","Crisp","Dooly","Harris","Macon","Marion","Muscogee","Quitman","Randolph","Schley","Stewart","Sumter","Talbot","Taylor","Webster"]},
+{"id":11,"name":"Southern Georgia","counties":["Atkinson","Bacon","Ben Hill","Berrien","Brantley","Brooks","Charlton","Clinch","Coffee","Cook","Echols","Irwin","Lanier","Lowndes","Pierce","Tift","Turner","Ware"]},
+{"id":10,"name":"Southwest Georgia","counties":["Baker","Calhoun","Colquitt","Decatur","Dougherty","Early","Grady","Lee","Miller","Mitchell","Seminole","Terrell","Thomas","Worth"]},
+{"id":4,"name":"Three Rivers","counties":["Butts","Carroll","Coweta","Heard","Lamar","Meriwether","Pike","Spalding","Troup","Upson"]}]
