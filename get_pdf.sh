8c90d174f9a49efe72b98baf0111ee803480c339
Landon Reed <landontreed@gmail.com>
2015-02-23 20:03:09 -0500

    updated pdf script

--- get_pdf.sh
+++ get_pdf.sh
@@ -1,14 +1,26 @@
 declare -A array
+# array=([county]=GEOID10)
 array=([congress]=DISTRICT [senate]=DISTRICT [house]=DISTRICT [county]=GEOID10 [rc]=acronym)
 
 for i in "${!array[@]}"
 do
 	echo "key  : $i"
 	echo "value: ${array[$i]}"
-	for key in $(jq -r ".objects[].geometries[] | .properties.${array[$i]}" "data/topo/$i.json"); do 
-		echo $i
-		echo $key
-		wkhtmltopdf --javascript-delay 1000  "127.0.0.1:3000/#$i/$key/funding/pdf" "pdf/$i/$key.pdf"; 
+	count=$(jq -r ".objects[].geometries | length" "data/topo/$i.json")
+	echo "count: $count"
+	for((j=0;j<$count;j++)); do
+		id="${array[$i]}"
+		name="${array[$i]}"
+		if [ "$i" == "county" ]; then
+			name="name"
+		fi
+		echo $name
+		idVal="$(jq -r ".objects[].geometries[$j].properties.$id" "data/topo/$i.json")"
+		nameVal="$(jq -r ".objects[].geometries[$j].properties.$name" "data/topo/$i.json")"
+		echo $id
+		echo $name
+		wkhtmltopdf --javascript-delay 1000  "127.0.0.1:3000/#$i/$idVal/funding/pdf" "pdf/$i/$nameVal.pdf"; 
+
 	done
 done
 
